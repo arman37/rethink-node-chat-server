d49b52601f93f08d5b52c46c9a47323d46d6c1ff
arman37 <arman.it37@gmail.com>
2017-11-29 17:37:57 +0600

    Update init-dev scripts
    Update rethinkdb initialization scripts so that thinky automatically create a new database for the first time

--- scripts/init-dev.js
+++ scripts/init-dev.js
@@ -5,15 +5,19 @@
  */
 'use strict';
 
+const thinky = require('../src/thinky');
 const cleanDB = require('./clean-db');
 const populateDB = require('./populate-db');
 
-Promise
-    .resolve(cleanDB())
-    .then(populateDB)
-    .then(() => {
-        console.log('\n\n DB initialization finished successfully!!!');
-    })
-    .catch((err) => {
-        console.log('\n\n Oops! DB initialization failed terribly!!!\n', 'error: ', err);
-    });
+thinky
+  .dbReady()
+  .then(cleanDB)
+  .then(populateDB)
+  .then(() => {
+    console.log('\n\n DB initialization finished successfully!!!');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log('\n\n Oops! DB initialization failed terribly!!!\n', 'error: ', err);
+    process.exit(0);
+  });
