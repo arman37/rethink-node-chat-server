e56531b1a1f77936d1fb4b5cac47cc5f752c26f2
arman37 <arman.it37@gmail.com>
2018-08-01 16:40:13 +0600

    Added event handler for signaling server

--- src/socket/socket-handler.js
+++ src/socket/socket-handler.js
@@ -7,6 +7,7 @@
 
 const appConstant = require('../app-constant');
 const connectedUsers = {};
+const connections = {};
 let io, socket;
 
 let onConnection = function(sckt) {
@@ -18,6 +19,61 @@ let onConnection = function(sckt) {
   socket.on(appConstant.CLIENT_CONNECTION, _handleClientConnect.bind(null, connectedUser));
 
   socket.on(appConstant.DISCONNECT, _handleClientDisconnect.bind(null, connectedUser));
+
+  socket.on('candidate', _handleCandidate);
+
+  socket.on('offer', _handleCallOffer);
+
+  socket.on('answer', _handleAnswer);
+
+  socket.on('leave', _handleLeave);
+};
+
+let _handleCandidate = (data) => {
+  console.log("Sending candidate to callee: ", data.callee);
+  let connection = connections[data.callee];
+
+  if (connection) {
+    connection.emit('candidate', {
+      candidate: data.candidate
+    });
+  }
+};
+
+let _handleCallOffer = (data) => {
+  console.log("Sending offer to callee: ", data.callee);           //for ex. UserA wants to call UserB
+
+  let connection = connections[data.callee];          //if UserB exists then send him offer details
+
+  if (connection) {
+    connection.emit('offer', {
+      offer: data.offer,
+      callee: data.callee,
+      caller: data.caller
+    });
+  }
+};
+
+let _handleAnswer = (data) => {
+  console.log("Sending answer to caller: ", data.caller);
+  //for ex. UserB answers UserA
+  let connection = connections[data.caller];
+
+  if (connection) {
+    connection.emit('answer', {
+      answer: data.answer
+    });
+  }
+};
+
+let _handleLeave = (data) => {
+  console.log("Disconnecting from ", data.name);
+  let connection = connections[data.name];
+
+  //notify the other user so he can disconnect his peer connection
+  if(connection) {
+    connection.emit('leave');
+  }
 };
 
 let _handleClientConnect = (connectedUser, userId) => {
@@ -28,6 +84,7 @@ let _handleClientConnect = (connectedUser, userId) => {
         connectedUser.id = user.id;
         connectedUser.username = user.username;
         connectedUsers[user.id] = connectedUser;
+        connections[user.username] = socket;
         io.sockets.emit(appConstant.NEW_USER_CONNECTION, connectedUsers);
       }
     })
